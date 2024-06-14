98afc8aa594853882de8d5cab2f5af8414b82ce2
Elias <emangulo1@gmail.com>
2024-06-13 17:15:31 -0700

    Delete js files

--- app.js
+++
@@ -1,9 +0,0 @@
-// User inputs:
-const nameFilter = ""; // "" or JobFilters or Jobs
-
-const inputFolder = "./input";
-const outputFile = `./output/output-${nameFilter}.csv`;
-
-// Program start. DO NOT MODIFY
-const { combineTextFiles } = require("./src/main");
-combineTextFiles(inputFolder, outputFile, nameFilter);
